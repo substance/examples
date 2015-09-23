d184069137a19af654deac7d789a9a992807febe
Michael Aufreiter <michael@substance.io>
2015-09-23 17:13:06 +0200

    Be explicit about available commands in TopicWriter.

--- topic-writer/commands/index.js
+++ topic-writer/commands/index.js
@@ -1,3 +1,16 @@
-var commands = require('substance/ui/commands');
-commands.push(require('./toggle_topic_citation'));
+var commands = [
+  require('substance/ui/commands/undo'),
+  require('substance/ui/commands/redo'),
+  require('substance/ui/commands/save'),
+  require('substance/ui/commands/make_paragraph'),
+  require('substance/ui/commands/make_heading1'),
+  require('substance/ui/commands/make_heading2'),
+  require('substance/ui/commands/make_heading3'),
+  require('substance/ui/commands/make_blockquote'),
+  require('substance/ui/commands/toggle_strong'),
+  require('substance/ui/commands/toggle_emphasis'),
+  require('substance/ui/commands/toggle_link'),
+  require('./toggle_topic_citation')
+];
+
 module.exports = commands;
