7422b62f199a0ec1995f3f58b70d7b78617f3851
Michael Aufreiter <michael@substance.io>
2016-02-06 19:11:55 +0100

    Towards a functional hub emulation example.

--- collabwriter/app.js
+++ collabwriter/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var doc1 = require('../simple/exampleDoc');
-var doc2 = require('../simple/exampleDoc2');
+var exampleDoc = require('../simple/exampleDoc');
 var MessageQueue = require('substance/util/MessageQueue');
 var CollabSession = require('substance/model/CollabSession');
+var StubHub = require('substance/util/StubHub');
 var Component = require('substance/ui/Component');
 var SplitPane = require('substance/ui/SplitPane');
 var Editor = require('../simple/Editor');
@@ -34,11 +34,21 @@ Component.extend(TwoEditors);
 
 window.onload = function() {
   var messageQueue = new MessageQueue();
+
+  // Two edited docs, one doc instance on the hub all with the same contents,
+  // now we start synchronizing them.
+
+  var doc1 = exampleDoc();
+  var doc2 = exampleDoc();
+  var hubDoc = exampleDoc();
+  
+  var hub = new StubHub(hubDoc, messageQueue);
+
   // Create two CollabSessions for the same doc
   var session1 = new CollabSession(doc1, {messageQueue: messageQueue});
-  var session2 = new CollabSession(doc2, {messageQueue: messageQueue});
-  TwoEditors.static.mount({
-    session1: session1,
-    session2: session2
-  }, 'body');
+  // var session2 = new CollabSession(doc2, {messageQueue: messageQueue});
+  // TwoEditors.static.mount({
+  //   session1: session1,
+  //   session2: session2
+  // }, 'body');
 };
--- simple/exampleDoc.js
+++ simple/exampleDoc.js
@@ -1,61 +1,65 @@
 var Article = require('../simple/Article');
-var doc = new Article();
-var body = doc.get('body');
 
-doc.create({
-  id: 'h1',
-  type: 'heading',
-  level: 1,
-  content: 'A Dream Within a Dream'
-});
-body.show('h1');
+function exampleDoc() {
+  var doc = new Article();
+  var body = doc.get('body');
 
-doc.create({
-  id: 'h2',
-  type: 'heading',
-  level: 2,
-  content: 'by Edgar Allan Poe'
-});
-body.show('h2');
+  doc.create({
+    id: 'h1',
+    type: 'heading',
+    level: 1,
+    content: 'A Dream Within a Dream'
+  });
+  body.show('h1');
 
-doc.create({
-  id: 'p1',
-  type: 'paragraph',
-  content: [
-    "Take this kiss upon the brow!",
-    "And, in parting from you now,",
-    "Thus much let me avow--",
-    "You are not wrong, who deem",
-    "That my days have been a dream;",
-    "Yet if hope has flown away",
-    "In a night, or in a day,",
-    "In a vision, or in none,",
-    "Is it therefore the less gone?",
-    "All that we see or seem",
-    "Is but a dream within a dream."
-  ].join("\n")
-});
-body.show('p1');
+  doc.create({
+    id: 'h2',
+    type: 'heading',
+    level: 2,
+    content: 'by Edgar Allan Poe'
+  });
+  body.show('h2');
 
-doc.create({
-  id: 'p2',
-  type: 'paragraph',
-  content: [
-    "I stand amid the roar",
-    "Of a surf-tormented shore,",
-    "And I hold within my hand",
-    "Grains of the golden sand--",
-    "How few! yet how they creep",
-    "Through my fingers to the deep,",
-    "While I weep--while I weep!",
-    "O God! can I not grasp",
-    "Them with a tighter clasp?",
-    "O God! can I not save",
-    "One from the pitiless wave?",
-    "Is all that we see or seem",
-    "But a dream within a dream?"
-  ].join("\n")
-});
-body.show('p2');
+  doc.create({
+    id: 'p1',
+    type: 'paragraph',
+    content: [
+      "Take this kiss upon the brow!",
+      "And, in parting from you now,",
+      "Thus much let me avow--",
+      "You are not wrong, who deem",
+      "That my days have been a dream;",
+      "Yet if hope has flown away",
+      "In a night, or in a day,",
+      "In a vision, or in none,",
+      "Is it therefore the less gone?",
+      "All that we see or seem",
+      "Is but a dream within a dream."
+    ].join("\n")
+  });
+  body.show('p1');
 
-module.exports = doc;
+  doc.create({
+    id: 'p2',
+    type: 'paragraph',
+    content: [
+      "I stand amid the roar",
+      "Of a surf-tormented shore,",
+      "And I hold within my hand",
+      "Grains of the golden sand--",
+      "How few! yet how they creep",
+      "Through my fingers to the deep,",
+      "While I weep--while I weep!",
+      "O God! can I not grasp",
+      "Them with a tighter clasp?",
+      "O God! can I not save",
+      "One from the pitiless wave?",
+      "Is all that we see or seem",
+      "But a dream within a dream?"
+    ].join("\n")
+  });
+  body.show('p2');
+  return doc;
+}
+
+module.exports = exampleDoc;
