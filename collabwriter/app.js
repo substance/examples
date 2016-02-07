9305d3810bbd3bc8d7887e6250259b8c47bc10a1
Michael Aufreiter <michael@substance.io>
2016-02-07 14:36:13 +0100

    Reactivate UI for Collabwriter example.

--- collabwriter/app.js
+++ collabwriter/app.js
@@ -46,9 +46,10 @@ window.onload = function() {
 
   // Create two CollabSessions for the same doc
   var session1 = new CollabSession(doc1, {messageQueue: messageQueue});
-  // var session2 = new CollabSession(doc2, {messageQueue: messageQueue});
-  // TwoEditors.static.mount({
-  //   session1: session1,
-  //   session2: session2
-  // }, 'body');
+  var session2 = new CollabSession(doc2, {messageQueue: messageQueue});
+  
+  TwoEditors.static.mount({
+    session1: session1,
+    session2: session2
+  }, 'body');
 };
