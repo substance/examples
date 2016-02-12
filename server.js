5941307c7ef09d43c8db3f1fdfbf5ea6696414b7
Daniel Nephin <dnephin@gmail.com>
2016-02-12 01:09:12 -0500

    Add docker-compose.yml
    Support running on 0.0.0.0 in a container
    
    Signed-off-by: Daniel Nephin <dnephin@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var port = process.env.PORT || 5000;
+var host = process.env.HOST || '127.0.0.1';
 var server = require('substance/util/server');
 
 // For each example we need those two lines
@@ -12,9 +13,10 @@ server.serveJS(app, '/notepad/app.js', path.join(__dirname, 'notepad', 'app.js')
 app.use(express.static(path.join(__dirname)));
 app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
 
+
 app.listen(port, function() {
   console.log("Substance Examples running on port " + port);
-  console.log("http://127.0.0.1:"+port+"/");
+  console.log(`http://${host}:${port}/`);
 });
 
 // Export app for requiring in test files
