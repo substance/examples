b25b28a443baac9741bbf04ecd39fc23463e2fc2
Daniel Beilinson <beilinson@gmail.com>
2016-02-28 13:38:49 +0300

    metadata schema (WIP)

---
+++ metadata/metadata.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var DocumentNode = require('substance/model/DocumentNode');
+
+function Metadata() {
+  Metadata.super.apply(this, arguments);
+}
+
+DocumentNode.extend(Metadata);
+
+Metadata.static.name = "metadata";
+
+Metadata.static.defineSchema({
+  author: { type: "string", default: "" },
+  published: { type: "boolean", default: false },
+  source: { type: "string", default: "" },
+  email: { type: "string", default: "" },
+  publishedAt: { type: "string", optional: true },
+  pages: { type: "number", optional: true },
+  century: { type: "string", optional: true },
+  genre: { type: "array", default: [] },
+  type: { type: "string", optional: true },
+  abstract: { type: "string", default: "" }
+});
+
+module.exports = Metadata;
