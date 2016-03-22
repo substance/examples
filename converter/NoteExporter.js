2b211d163fa24c7760505a658ce375d2ac4ab899
jmjf <jamee.mikell@gmail.com>
2016-03-22 10:32:31 -0400

    Make testNoteExporter work

--- converter/NoteExporter.js
+++ converter/NoteExporter.js
@@ -10,6 +10,17 @@ function NoteExporter() {
   });
 }
 
+NoteExporter.Prototype = function() {
+  this.convertDocument = function(doc) {
+    var body = doc.get('body');
+    var elements = this.convertContainer(body, this.state.containerId);
+    var out = elements.map(function(el) {
+      return el.outerHTML;
+    });
+    return out.join('');
+  };
+};
+
 HTMLExporter.extend(NoteExporter);
 
 module.exports = NoteExporter;
