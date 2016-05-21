8cb465d7e922bab87f1fb87be0fcb2dbf95dfad1
Michael Aufreiter <michael@substance.io>
2016-05-21 19:34:22 +0200

    Configure ProseEditor.

--- prose/app.js
+++ prose/app.js
@@ -1,25 +1,29 @@
 'use strict';
 
 var Component = require('substance/ui/Component');
-var Icon = require('substance/ui/FontAwesomeIcon');
 var ProseEditor = require('substance/packages/prose-editor/ProseEditor');
-
-var InsertImageTool = require('substance/packages/image/ImageTool');
-
 var example = require('substance/test/fixtures/collab/poem');
 
 var config = ProseEditor.static.mergeConfig(ProseEditor.static.config, {
   components: {
     'image': require('substance/packages/image/ImageComponent')
   },
+  commands: [
+    require('substance/packages/image/ImageCommand')
+  ],
   surfaces: {
-    bodyEditor: {
-      commands: [
-        require('substance/packages/image/ImageCommand')
-      ]
+    body: {
+      commands: ['image']
     }
-  }
+  },
+  tools: [
+    {
+      component: require('substance/packages/image/ImageTool'),
+      icon: 'fa-image'
+    }
+  ]
 });
+
 
 function App() {
   App.super.apply(this, arguments);
@@ -29,18 +33,10 @@ App.Prototype = function() {
 
   this.render = function($$) {
     var el = $$('div').addClass('app');
-
-    var editor = $$(ProseEditor, {
+    el.append($$(ProseEditor, {
       doc: this.props.doc,
       config: config
-    });
-    editor.outlet('tools').append(
-      // $$(InsertTableTool).append($$(Icon, {icon: 'fa-table'}))
-      $$(InsertImageTool).append($$(Icon, {icon: 'fa-image'}))
-    );
-
-    el.append(editor);
-
+    }));
     return el;
   };
 };
