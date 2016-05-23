cadde5c056118ec9deda9a80d8d33589b295e9bd
Michael Aufreiter <michael@substance.io>
2016-05-23 15:21:42 +0200

    Consider core functionality as just another Package.

--- prose/MyProseEditorConfig.js
+++ prose/MyProseEditorConfig.js
@@ -1,5 +1,9 @@
+var ProseEditorPackage = require('substance/packages/prose-editor/ProseEditorPackage');
 var ImagePackage = require('substance/packages/image/ImagePackage');
 
 module.exports = function(config) {
+  // Base package with default rich text nodes
+  config.import(ProseEditorPackage);
+  // Custom packages
   config.import(ImagePackage, {icon: 'fa-image'});
 };
