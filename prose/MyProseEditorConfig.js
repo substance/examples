7ba2d529f3bcb4703a311a1fcbef6b999f984243
Michael Aufreiter <michael@substance.io>
2016-05-27 14:49:34 +0200

    Activate save tool.

--- prose/MyProseEditorConfig.js
+++ prose/MyProseEditorConfig.js
@@ -1,11 +1,16 @@
 var ProseEditorPackage = require('substance/packages/prose-editor/ProseEditorPackage');
 var ImagePackage = require('substance/packages/image/ImagePackage');
 
-module.exports = function(config) {
+var SaveCommand = require('substance/ui/SaveCommand');
+var SaveTool = require('substance/ui/SaveTool');
 
+module.exports = function(config) {
   // Base package with default rich text nodes
   config.import(ProseEditorPackage);
   // Custom packages
   config.import(ImagePackage, {icon: 'fa-image'});
- 
+
+  // Activate save tool
+  config.addCommand(SaveCommand);
+  config.addTool(SaveTool, {icon: 'fa-save'});
 };
