c7d987a18365ff2e196d31f0b4fd3256ffb9a0c3
Michael Aufreiter <michael@substance.io>
2016-05-27 15:16:23 +0200

    Change layout of package definition.

--- prose/MyProseEditorConfig.js
+++ prose/MyProseEditorConfig.js
@@ -4,13 +4,16 @@ var ImagePackage = require('substance/packages/image/ImagePackage');
 var SaveCommand = require('substance/ui/SaveCommand');
 var SaveTool = require('substance/ui/SaveTool');
 
-module.exports = function(config) {
-  // Base package with default rich text nodes
-  config.import(ProseEditorPackage);
-  // Custom packages
-  config.import(ImagePackage, {icon: 'fa-image'});
+module.exports = {
+  name: 'my-prose-editor',
+  configure: function(config) {
+    // Base package with default rich text nodes
+    config.import(ProseEditorPackage);
+    // Custom packages
+    config.import(ImagePackage, {icon: 'fa-image'});
 
-  // Activate save tool
-  config.addCommand(SaveCommand);
-  config.addTool(SaveTool, {icon: 'fa-save'});
+    // Activate save tool
+    config.addCommand(SaveCommand);
+    config.addTool(SaveTool, {icon: 'fa-save'});
+  }
 };
