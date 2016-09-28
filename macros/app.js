79be27a62ab4237f3e129e23edd8dd22edc0a3fe
Michael Aufreiter <michael@substance.io>
2016-09-28 17:05:05 +0200

    Revive Macros example.

--- macros/app.js
+++ macros/app.js
@@ -1,7 +1,44 @@
-'use strict';
+const {
+  ProseEditor, ProseEditorConfigurator, DocumentSession,
+  ProseEditorPackage, HeadingMacro
+} = substance
 
-var proseExample = require('../lib/proseExample');
-var fixture = require('./fixture');
-var config = require('./config');
+/*
+  Example document
+*/
+const fixture = function(tx) {
+  let body = tx.get('body')
+  tx.create({
+    id: 'p1',
+    type: 'paragraph',
+    content: "Type # followed by a space to create a heading!"
+  })
+  body.show('p1')
+  tx.create({
+    id: 'p2',
+    type: 'paragraph',
+    content: ""
+  })
+  body.show('p2')
+}
 
-proseExample(fixture, config);
+/*
+  Application
+*/
+let config = {
+  name: 'macros-example',
+  configure: function(config) {
+    config.import(ProseEditorPackage)
+    config.addMacro(HeadingMacro)
+  }
+}
+let configurator = new ProseEditorConfigurator().import(config)
+
+window.onload = function() {
+  let doc = configurator.createArticle(fixture)
+  let documentSession = new DocumentSession(doc)
+  ProseEditor.mount({
+    documentSession: documentSession,
+    configurator: configurator
+  }, document.body)
+}
