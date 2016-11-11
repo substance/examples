59ac121b742cc56402c16bb2c1e2ee50f86b8772
Michael Aufreiter <michael@substance.io>
2016-11-11 16:53:38 +0100

    Use file nodes.

--- image/app.js
+++ image/app.js
@@ -14,10 +14,17 @@ const fixture = function(tx) {
     content: "Insert a new image using the image tool."
   })
   body.show('p1')
+
+  tx.create({
+    id: 'f1',
+    type: 'file',
+    fileType: 'image',
+    url: 'https://pbs.twimg.com/profile_images/706616363599532032/b5z-Hw5g.jpg'
+  })
   tx.create({
     id: 'i1',
     type: 'image',
-    src: "https://pbs.twimg.com/profile_images/706616363599532032/b5z-Hw5g.jpg"
+    imageFile: 'f1'
   })
   body.show('i1')
   tx.create({
@@ -29,10 +36,9 @@ const fixture = function(tx) {
   tx.create({
     id: 'i2',
     type: 'image',
-    src: "https://pbs.twimg.com/profile_images/706616363599532032/b5z-Hw5g.jpg"
+    imageFile: 'f1'
   })
   body.show('i2')
-
 }
 
 /*
