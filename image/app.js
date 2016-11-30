b62e806ad87fc74671f0a2821499951a085ed0c2
Roland Quast <roland@formreturn.com>
2016-11-30 12:06:36 +1000

    Show how to use the save handler
    This change explains how the save handler works a bit more clearly.

--- image/app.js
+++ image/app.js
@@ -5,11 +5,32 @@ import {
 
 import fixture from './fixture'
 
+class SaveHandlerStub {
+
+  /*
+    Saving a document involves two steps.
+    - syncing files (e.g. images) with a backend
+    - storing a snapshot of the document's content (e.g. a XML serialization)
+  */
+  saveDocument(params) {
+    console.info('Simulating save ...', params)
+
+    return params.fileManager.sync()
+    .then(() => {
+      // Here you would run a converter (HTML/XML) usually
+      // and send the result to a REST endpoint.
+      console.info('Creating document snapshot...')
+    })
+
+  }
+}
+
 let cfg = new ProseEditorConfigurator()
 cfg.import(ProseEditorPackage)
 cfg.import(ImagePackage)
 // Enable save button
 cfg.import(PersistencePackage)
+cfg.setSaveHandlerClass(SaveHandlerStub)
 
 window.onload = function() {
   let doc = cfg.createArticle(fixture)
