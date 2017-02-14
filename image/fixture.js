8fdd7d1d377e5197e41548169d295c8522647a65
Michael Aufreiter <michael@substance.io>
2017-02-14 19:24:16 +0100

    Local cat.

--- image/fixture.js
+++ image/fixture.js
@@ -6,12 +6,11 @@ export default function(tx) {
     content: "Insert a new image using the image tool."
   })
   body.show('p1')
-
   tx.create({
     id: 'f1',
     type: 'file',
     fileType: 'image',
-    url: 'https://pbs.twimg.com/profile_images/706616363599532032/b5z-Hw5g.jpg'
+    url: './assets/cat.jpg'
   })
   tx.create({
     id: 'i1',
@@ -25,10 +24,4 @@ export default function(tx) {
     content: "Please note that images are not actually uploaded in this example. You would need to provide a custom file client that talks to an image store. See FileClientStub which reveals the API you have to implement."
   })
   body.show('p2')
-  tx.create({
-    id: 'i2',
-    type: 'image',
-    imageFile: 'f1'
-  })
-  body.show('i2')
 }
